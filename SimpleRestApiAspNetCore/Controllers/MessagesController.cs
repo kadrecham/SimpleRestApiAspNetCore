314d2dcd785eb2a50eecc0bdf89f2e48f3b8e82d
kadrecham <kadrecham@hotmail.com>
2019-12-13 08:48:59 +0100

    Add project files.

---
+++ SimpleRestApiAspNetCore/Controllers/MessagesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleRestApiAspNetCore.Models;
+
+namespace SimpleRestApiAspNetCore.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly MessageContext _context;
+
+        public MessagesController(MessageContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: api/Messages/5
+        [HttpGet("{start:min(0)?}")]
+        public async Task<ActionResult<IEnumerable<Message>>> GetMessages(int start)
+        {
+            return await _context.Messages.Skip(start).Take(Constants.MAX_RETURNED_RECORDS).ToListAsync();
+        }
+        
+
+        // POST: api/Messages
+        [HttpPost]
+        public async Task<ActionResult<Message>> PostMessage(Message message)
+        {
+            _context.Messages.Add(message);
+            await _context.SaveChangesAsync();
+
+            return Ok(message);
+        }
+
+        
+    }
+}
---
+++ SimpleRestApiAspNetCore/Models/MessageContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleRestApiAspNetCore.Models
+{
+    public class MessageContext : DbContext
+    {
+        public MessageContext(DbContextOptions<MessageContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Message> Messages { get; set; }
+    }
+}
+
